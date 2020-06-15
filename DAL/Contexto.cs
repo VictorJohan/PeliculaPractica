23c3215b65c0582cf37c04b1f03f72ff80e380d5
Victor Johan <Vicjohan23@gmail.com>
2020-06-15 15:59:51 -0400

    SE AGREGO LA CARPETA DALL Y DATA

---
+++ DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroPeliculas.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegistroPeliculas.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Peliculas> Peliculas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Date Resource= DATA\Peliculas.db");
+        }
+    }
+}
---
+++ Entidades/Peliculas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Windows.Controls;
+
+namespace RegistroPeliculas.Entidades
+{
+    public class Peliculas
+    {
+        [Key]
+        public int IdPelicula { get; set; }
+        public string NombrePelicula { get; set; }
+        public string Sipnosis { get; set; }
+        public string Genero { get; set; }
+        public DateTime FechaEstreno { get; set; } = DateTime.Now;
+    }
+}
